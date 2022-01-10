8d2ce031fe3b74618f4de3aefef044905f28bc09
Matias Olivera <moliverafreire@gmail.com>
2022-01-10 15:24:38 -0300

    Add function to post message to account

--- programs/tutorial/src/lib.rs
+++ programs/tutorial/src/lib.rs
@@ -5,10 +5,50 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod tutorial {
     use super::*;
+
     pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn post_message(ctx: Context<PostMessage>, content: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        let message = Message {
+            author_address: *user.to_account_info().key,
+            content: content.to_string(),
+        };
+
+        base_account.message_list.push(message);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 10000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct PostMessage<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Message {
+    pub author_address: Pubkey,
+    pub content: String,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub message_list: Vec<Message>,
+}
--- tests/tutorial.js
+++ tests/tutorial.js
@@ -2,14 +2,36 @@
 
 const anchor = require('@project-serum/anchor');
 
+const { SystemProgram } = anchor.web3;
+
 const test = async () => {
   console.log('🚀 Starting test...');
 
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Tutorial;
-  const tx = await program.rpc.initialize();
+  const baseAccount = anchor.web3.Keypair.generate();
+  const tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
   console.log('📝 Your transaction signature', tx);
+
+  await program.rpc.postMessage('This is a test message!', {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  const account = await program.account.baseAccount.fetch(
+    baseAccount.publicKey,
+  );
+  console.log('👀 Message list', account.messageList);
 };
 
 const runTest = async () => {
