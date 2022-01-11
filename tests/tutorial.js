e19749183361358569f0b2395fb4ab83443eb89e
Matias Olivera <moliverafreire@gmail.com>
2022-01-11 09:52:19 -0300

    Update tests

--- tests/tutorial.js
+++ tests/tutorial.js
@@ -1,47 +1,93 @@
-/* eslint-disable no-console */
+const assert = require('assert');
 
 const anchor = require('@project-serum/anchor');
 
-const { SystemProgram } = anchor.web3;
-
-const test = async () => {
-  console.log('🚀 Starting test...');
-
-  const provider = anchor.Provider.env();
-  anchor.setProvider(provider);
-
+describe('tutorial', () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
   const program = anchor.workspace.Tutorial;
-  const baseAccount = anchor.web3.Keypair.generate();
-  const tx = await program.rpc.initialize({
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId,
-    },
-    signers: [baseAccount],
+
+  it('can post a new message', async () => {
+    // Call the `PostMessage` instruction.
+    const message = anchor.web3.Keypair.generate();
+    await program.rpc.postMessage('Hummus, am I right?', {
+      accounts: {
+        message: message.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [message],
+    });
+
+    // Fetch the account details of the created message.
+    const messageAccount = await program.account.message.fetch(
+      message.publicKey,
+    );
+
+    // Ensure it has the right data.
+    assert.equal(
+      messageAccount.author.toBase58(),
+      program.provider.wallet.publicKey.toBase58(),
+    );
+    assert.equal(messageAccount.content, 'Hummus, am I right?');
+    assert.ok(messageAccount.timestamp);
   });
-  console.log('📝 Your transaction signature', tx);
 
-  await program.rpc.postMessage('This is a test message!', {
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-    },
+  it('can post a new message from a different author', async () => {
+    // Generate another user and airdrop them some SOL.
+    const anotherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(
+      anotherUser.publicKey,
+      1000000000,
+    );
+    await program.provider.connection.confirmTransaction(signature);
+
+    // Call the `PostMessage` instruction on behalf of this other user.
+    const message = anchor.web3.Keypair.generate();
+    await program.rpc.postMessage('Yay Tofu!', {
+      accounts: {
+        message: message.publicKey,
+        author: anotherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [anotherUser, message],
+    });
+
+    // Fetch the account details of the created message.
+    const messageAccount = await program.account.message.fetch(
+      message.publicKey,
+    );
+
+    // Ensure it has the right data.
+    assert.equal(
+      messageAccount.author.toBase58(),
+      anotherUser.publicKey.toBase58(),
+    );
+    assert.equal(messageAccount.content, 'Yay Tofu!');
+    assert.ok(messageAccount.timestamp);
   });
-  const account = await program.account.baseAccount.fetch(
-    baseAccount.publicKey,
-  );
-  console.log('👀 Message list', account.messageList);
-};
 
-const runTest = async () => {
-  try {
-    await test();
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-runTest();
+  it('cannot provide a content with more than 280 characters', async () => {
+    try {
+      const message = anchor.web3.Keypair.generate();
+      const contentWith281Chars = 'x'.repeat(281);
+      await program.rpc.postMessage(contentWith281Chars, {
+        accounts: {
+          message: message.publicKey,
+          author: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [message],
+      });
+    } catch (error) {
+      assert.equal(
+        error.msg,
+        'The provided content should be 280 characters long maximum.',
+      );
+      return;
+    }
+    assert.fail(
+      'The instruction should have failed with a 281-character content.',
+    );
+  });
+});
