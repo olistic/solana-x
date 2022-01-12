d4505290601ec6f74d27f239ee71b08b2ef31920
Matias Olivera <moliverafreire@gmail.com>
2022-01-12 07:05:26 -0300

    Update ESLint config

--- web/src/utils/avatars.js
+++ web/src/utils/avatars.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/prefer-default-export */
-
 const BASE_URL = 'https://avatars.dicebear.com/api';
 const TYPE = 'pixel-art-neutral';
 
--- web/src/utils/publicKeys.js
+++ web/src/utils/publicKeys.js
@@ -1,4 +1,2 @@
-/* eslint-disable import/prefer-default-export */
-
 export const condensePublicKey = (publicKey) =>
   `${publicKey.slice(0, 4)}..${publicKey.slice(-4)}`;
