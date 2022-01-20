bec75b79dfe47234380f1dac0c17609e28ff0d60
Matias Olivera <moliverafreire@gmail.com>
2022-01-19 21:18:14 -0300

    Animate back button

--- web/src/components/BackButton.js
+++ web/src/components/BackButton.js
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 
 import { styled } from '../stitches.config';
 
+const Icon = styled('span', {
+  display: 'block',
+});
+
 const StyledButton = styled('button', {
   border: 'none',
   cursor: 'pointer',
@@ -12,16 +16,30 @@ const StyledButton = styled('button', {
   padding: 0,
   size: '$4',
   textAlign: 'left',
+  transform: 'translateX(0)',
 
   '&:focus': {
     outline: 'none',
+  },
+
+  [`& > ${Icon}`]: {
+    transform: 'translateX(0)',
+    transition: 'transform 150ms ease',
+  },
+
+  [`&:hover > ${Icon}`]: {
+    transform: 'translateX(-4px)',
   },
 });
 
 function BackButton() {
   const navigate = useNavigate();
 
-  return <StyledButton onClick={() => navigate(-1)}>⟵</StyledButton>;
+  return (
+    <StyledButton onClick={() => navigate(-1)}>
+      <Icon>⟵</Icon>
+    </StyledButton>
+  );
 }
 
 export default BackButton;
